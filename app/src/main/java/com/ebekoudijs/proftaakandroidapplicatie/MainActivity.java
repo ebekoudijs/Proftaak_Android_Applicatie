f4c374671c5762c58877a317ca676e6f2418d060
PhilipLenssen <philiplenssen@gmail.com>
2020-12-03 09:32:40 +0100

    account creatie

--- app/src/main/java/com/ebekoudijs/proftaakandroidapplicatie/MainActivity.java
+++ app/src/main/java/com/ebekoudijs/proftaakandroidapplicatie/MainActivity.java
@@ -2,7 +2,25 @@ package com.ebekoudijs.proftaakandroidapplicatie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.DataOutputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +28,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        EditText EditTextusername = findViewById(R.id.editTextUsername);
+        EditText EditTextpassword = findViewById(R.id.editTextPassword);
+        EditText EditTextphoneNumber = findViewById(R.id.editTextPhoneNumber);
+        Button submit = findViewById(R.id.buttonCreate);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                User user = new User(EditTextusername.toString(), EditTextpassword.toString(), EditTextphoneNumber.toString());
+
+                try {
+                    URL url = new URL("145.93.128.64:51273/weatherforecast/post");
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("POST");
+                    conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+                    conn.setRequestProperty("Accept","application/json");
+                    conn.setDoOutput(true);
+                    conn.setDoInput(true);
+
+                    Gson gson = new Gson();
+                    String json = gson.toJson(user);
+
+                    Log.i("JSON", json);
+                    DataOutputStream os = new DataOutputStream(conn.getOutputStream());
+                    //os.writeBytes(URLEncoder.encode(jsonParam.toString(), "UTF-8"));
+                    os.writeBytes(json);
+
+                    os.flush();
+                    os.close();
+
+                    Log.i("STATUS", String.valueOf(conn.getResponseCode()));
+                    Log.i("MSG" , conn.getResponseMessage());
+
+                    conn.disconnect();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
     }
 }
